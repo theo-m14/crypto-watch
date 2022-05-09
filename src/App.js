5ba6da4f76ef7cf3ba7bede3abefcb8378df4886
Théo Michel <theo.michel2@outlook.fr>
2022-05-09 20:37:10 +0200

    affichage simple des cryptos

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import axios from "axios";
 import GlobalChart from "./components/GlobalChart";
 import HeaderInfo from "./components/HeaderInfo";
 import Table from "./components/Table";
+import ToTop from "./components/ToTop";
 
 function App() {
   const [coinsData, setCoinsData] = useState([]);
@@ -13,6 +14,14 @@ function App() {
         `https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=250&page=1&sparkline=false&price_change_percentage=1h%2C24h%2C7d%2C14d%2C30d%2C200d%2C1y`
       )
       .then((res) => setCoinsData(res.data));
+
+    window.addEventListener("scroll", () => {
+      if (window.scrollY > 145) {
+        document.querySelector(".table-header").classList.add("active");
+      } else {
+        document.querySelector(".table-header").classList.remove("active");
+      }
+    });
   }, []);
 
   return (
@@ -22,6 +31,7 @@ function App() {
         <GlobalChart coinsData={coinsData} />
       </header>
       <Table coinsData={coinsData} />
+      <ToTop />
     </div>
   );
 }
